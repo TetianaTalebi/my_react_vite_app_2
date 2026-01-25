98c343102a5e2443e28f2f21405dbfa2cd693025
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2026-01-25 18:21:52 -0500

    Create "QuoteFetcher" component

--- my_react_vite_app_2/src/App.jsx
+++ my_react_vite_app_2/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import CounterWithUseEffect from './CounterWithUseEffect'
+import QuoteFetcher from './QuoteFetcher'
 
 function App() {
 
   return (
     <>
-      <CounterWithUseEffect />
+      <QuoteFetcher />
     </>
   )
 }
